16b499d1521df98b9175150cfabcfbeadd632ef2
ryugahee <gagml910@hanmail.net>
2023-11-23 12:00:25 +0900

    feat: 아이템dto 추가

---
+++ src/main/java/com/fp/backend/dto/ItemDto.java
@@ -0,0 +1,32 @@
+package com.fp.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+public class ItemDto {
+    private Long id;
+
+    private String itemTitle;
+
+    private int minPrice;
+
+    private String itemDetail;
+
+    private int time;
+
+    private String itemTag;
+
+    private String itemType;
+
+    private int weight;
+
+    private boolean isSoldout;
+
+    private LocalDateTime regTime;
+
+    private LocalDateTime updateTime;
+}
