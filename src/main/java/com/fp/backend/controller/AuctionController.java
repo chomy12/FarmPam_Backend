0bdec8aeb8cc0ce203a3c79f5a19b0cca277e258
KKchan1119 <kkchan1119@gmail.com>
2023-11-23 11:55:13 +0900

    api connection test

--- src/main/java/com/fp/backend/controller/AuctionController.java
+++ src/main/java/com/fp/backend/controller/AuctionController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 public class AuctionController {
     @PostMapping("/bid")
     public String PostTest(@RequestBody String msg){
+        System.out.println("msg = "+ msg);
         return"post success"+msg;
     }
 }
