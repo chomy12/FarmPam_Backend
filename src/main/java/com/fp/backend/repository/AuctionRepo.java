d57a03342c824e1267d9e434826fae95ba76a25a
KKchan1119 <kkchan1119@gmail.com>
2023-11-27 15:35:35 +0900

    feature: dynamo dependencies and auction config _ing

---
+++ src/main/java/com/fp/backend/repository/AuctionRepo.java
@@ -0,0 +1,35 @@
+package com.fp.backend.repository;
+
+
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBMapper;
+import com.amazonaws.services.dynamodbv2.datamodeling.DynamoDBSaveExpression;
+import com.amazonaws.services.dynamodbv2.model.AttributeValue;
+import com.amazonaws.services.dynamodbv2.model.ExpectedAttributeValue;
+import com.fp.backend.entity.Auction;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class AuctionRepo {
+
+    private final DynamoDBMapper dynamoDBMapper;
+    //생성
+    public Auction saveAuction(Auction auction){
+        dynamoDBMapper.save(auction);
+        return auction;
+    }
+    //조회
+    public Auction getAuctionById(String auctionId){
+        return dynamoDBMapper.load(Auction.class, auctionId);
+    }
+    //수정
+    public String updateAuction(Auction auction){
+        dynamoDBMapper.save(auction, new DynamoDBSaveExpression()
+                .withExpectedEntry("auctionId", new ExpectedAttributeValue(
+                        new AttributeValue().withS(auction.getId())
+                )));
+        return auction.getId();
+    }
+}
--- src/main/java/com/fp/backend/system/config/FCMConfig.java
+++ src/main/java/com/fp/backend/system/config/FCMConfig.java
@@ -1,39 +1,39 @@
-//package com.fp.backend.system.config;
-//
-//import com.google.auth.oauth2.GoogleCredentials;
-//import com.google.firebase.FirebaseApp;
-//import com.google.firebase.FirebaseOptions;
-//import com.google.firebase.messaging.FirebaseMessaging;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.io.ClassPathResource;
-//
-//import java.io.IOException;
-//import java.io.InputStream;
-//import java.util.List;
-//
-//@Configuration
-//public class FCMConfig {
-//    @Bean
-//    FirebaseMessaging firebaseMessaging() throws IOException{
-//        ClassPathResource resource = new ClassPathResource("firebase/farmpam-4f9cc-firebase-adminsdk-rvg13-9e11712c15.json");
-//
-//        InputStream refreshToken = resource.getInputStream();
-//
-//        FirebaseApp firebaseApp = null;
-//        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
-//
-//        if(firebaseAppList != null && !firebaseAppList.isEmpty()){
-//            for(FirebaseApp app : firebaseAppList){
-//                if(app.getName().equals(firebaseApp.DEFAULT_APP_NAME)){
-//                    firebaseApp = app;
-//                }
-//            }
-//        } else{
-//            FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
-//            firebaseApp = FirebaseApp.initializeApp(options);
-//        }
-//
-//        return FirebaseMessaging.getInstance(firebaseApp);
-//    }
-//}
+package com.fp.backend.system.config;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import com.google.firebase.messaging.FirebaseMessaging;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+@Configuration
+public class FCMConfig {
+    @Bean
+    FirebaseMessaging firebaseMessaging() throws IOException{
+        ClassPathResource resource = new ClassPathResource("firebase/farmpam-4f9cc-firebase-adminsdk-rvg13-9e11712c15.json");
+
+        InputStream refreshToken = resource.getInputStream();
+
+        FirebaseApp firebaseApp = null;
+        List<FirebaseApp> firebaseAppList = FirebaseApp.getApps();
+
+        if(firebaseAppList != null && !firebaseAppList.isEmpty()){
+            for(FirebaseApp app : firebaseAppList){
+                if(app.getName().equals(firebaseApp.DEFAULT_APP_NAME)){
+                    firebaseApp = app;
+                }
+            }
+        } else{
+            FirebaseOptions options = FirebaseOptions.builder().setCredentials(GoogleCredentials.fromStream(refreshToken)).build();
+            firebaseApp = FirebaseApp.initializeApp(options);
+        }
+
+        return FirebaseMessaging.getInstance(firebaseApp);
+    }
+}
